f684dcb94443398bc3cb43b0ad2d06625dd0bd9f
mannsoj <100628167+mannsoj@users.noreply.github.com>
2023-01-27 10:39:13 +0100

    Update Form1.cs
    do no chen dm jajaj

--- FormsApp/FormsApp/Form1.cs
+++ FormsApp/FormsApp/Form1.cs
@@ -81,6 +81,21 @@ void kresliAjajaj3()
                     //grafika.FillRectangle(maroonBrush, 0, 0, sizeExtension * i, sizeExtension * i);
                 }
             }
+void kresliAjajaj4()
+            {
+                int size = pictrOutput.Width;
+                int sizeExtension = size / 10;
+
+                for (int i = 0; i <= 10; i++)
+                {
+                    limeP.Color = Color.FromArgb(rnd.Next(0, 256), rnd.Next(0, 256), rnd.Next(0, 256));
+                    grafika.DrawRectangle(limeP, 0, 0+(sizeExtension*i), size-(sizeExtension*i), size-(sizeExtension*i));
+                    //maroonBrush.Color 
+                    Brush maroonBrush = new SolidBrush(Color.FromArgb(rnd.Next(0, 256), rnd.Next(0, 256), rnd.Next(0, 256)));
+                    grafika.FillRectangle(maroonBrush, 0, 0, sizeExtension * i, sizeExtension * i);
+                    //grafika.FillRectangle(maroonBrush, 0, 0, sizeExtension * i, sizeExtension * i);
+                }
+            }
 
         }
     }
