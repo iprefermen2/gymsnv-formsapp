1a36ed3da5c48832a985ffb0bd9fccc815239b06
augustinromaniak <augustin.romaniak88@zoznam.sk>
2023-01-27 11:39:07 +0100

    start

--- FormsApp/FormsApp/Form1.Designer.cs
+++ FormsApp/FormsApp/Form1.Designer.cs
@@ -30,9 +30,6 @@
         {
             this.btnStart = new System.Windows.Forms.Button();
             this.pictrOutput = new System.Windows.Forms.PictureBox();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictrOutput)).BeginInit();
             this.SuspendLayout();
             // 
@@ -45,7 +42,7 @@
             this.btnStart.Name = "btnStart";
             this.btnStart.Size = new System.Drawing.Size(138, 46);
             this.btnStart.TabIndex = 0;
-            this.btnStart.Text = "LH";
+            this.btnStart.Text = "Zmaž";
             this.btnStart.UseVisualStyleBackColor = false;
             this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
             // 
@@ -58,51 +55,9 @@
             this.pictrOutput.TabIndex = 1;
             this.pictrOutput.TabStop = false;
             // 
-            // button2
-            // 
-            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
-            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.button2.ForeColor = System.Drawing.Color.IndianRed;
-            this.button2.Location = new System.Drawing.Point(12, 102);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(138, 46);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "PH";
-            this.button2.UseVisualStyleBackColor = false;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // button3
-            // 
-            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
-            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.button3.ForeColor = System.Drawing.Color.IndianRed;
-            this.button3.Location = new System.Drawing.Point(12, 164);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(138, 46);
-            this.button3.TabIndex = 3;
-            this.button3.Text = "PD";
-            this.button3.UseVisualStyleBackColor = false;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
-            // button4
-            // 
-            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
-            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.button4.ForeColor = System.Drawing.Color.IndianRed;
-            this.button4.Location = new System.Drawing.Point(12, 228);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(138, 46);
-            this.button4.TabIndex = 4;
-            this.button4.Text = "LD";
-            this.button4.UseVisualStyleBackColor = false;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
-            // 
             // Form1
             // 
             this.ClientSize = new System.Drawing.Size(596, 465);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
             this.Controls.Add(this.pictrOutput);
             this.Controls.Add(this.btnStart);
             this.Name = "Form1";
@@ -117,9 +72,6 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button btnStart;
         private System.Windows.Forms.PictureBox pictrOutput;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
     }
 }
 
