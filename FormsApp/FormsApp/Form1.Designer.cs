42388e03545c3311e2ab679a89c7f4c2ecdc7436
augustinromaniak <augustin.romaniak88@zoznam.sk>
2023-01-27 12:18:41 +0100

    this shit ain't working

--- FormsApp/FormsApp/Form1.Designer.cs
+++ FormsApp/FormsApp/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace FormsApp
 {
-    partial class Form1
+    partial class workForm
     {
         /// <summary>
         /// Required designer variable.
@@ -30,6 +30,7 @@
         {
             this.btnStart = new System.Windows.Forms.Button();
             this.pictrOutput = new System.Windows.Forms.PictureBox();
+            this.buttonColor = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictrOutput)).BeginInit();
             this.SuspendLayout();
             // 
@@ -54,13 +55,29 @@
             this.pictrOutput.Size = new System.Drawing.Size(400, 400);
             this.pictrOutput.TabIndex = 1;
             this.pictrOutput.TabStop = false;
+            this.pictrOutput.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictrOutput_MouseDown);
+            this.pictrOutput.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictrOutput_MouseMove);
+            this.pictrOutput.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictrOutput_MouseUp);
             // 
-            // Form1
+            // buttonColor
+            // 
+            this.buttonColor.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.buttonColor.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.buttonColor.Location = new System.Drawing.Point(21, 89);
+            this.buttonColor.Name = "buttonColor";
+            this.buttonColor.Size = new System.Drawing.Size(129, 348);
+            this.buttonColor.TabIndex = 2;
+            this.buttonColor.Text = "Farba\r\n";
+            this.buttonColor.UseVisualStyleBackColor = false;
+            this.buttonColor.Click += new System.EventHandler(this.buttonColor_Click);
+            // 
+            // workForm
             // 
             this.ClientSize = new System.Drawing.Size(596, 465);
+            this.Controls.Add(this.buttonColor);
             this.Controls.Add(this.pictrOutput);
             this.Controls.Add(this.btnStart);
-            this.Name = "Form1";
+            this.Name = "workForm";
             ((System.ComponentModel.ISupportInitialize)(this.pictrOutput)).EndInit();
             this.ResumeLayout(false);
 
@@ -68,10 +85,10 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
+
         private System.Windows.Forms.Button btnStart;
         private System.Windows.Forms.PictureBox pictrOutput;
+        private System.Windows.Forms.Button buttonColor;
     }
 }
 
--- FormsApp/FormsApp/Form1.cs
+++ FormsApp/FormsApp/Form1.cs
@@ -10,24 +10,65 @@ using System.Windows.Forms;
 
 namespace FormsApp
 {
-    public partial class Form1 : Form
+    public partial class workForm : Form
     {
-        public Form1()
+        bool stavKreslenia = false;
+        int mouseOldX = 0;
+        int mouseOldY = 0;
+        public workForm()
         {
             //pri vzniku okna
             InitializeComponent();
         }
 
-        private void btnStart_Click(object sender, EventArgs e)
+        public void kresli(MouseEventArgs e)
         {
-
             Graphics grafika = pictrOutput.CreateGraphics();
-            Random rnd = new Random();
+            //Random rnd = new Random();
 
-            Pen limeP = new Pen(Color.Lime, 10);
-                             
+            Pen coloredPen = new Pen(Color.Lime, 20);
+            int  mouseFetchX, mouseFetchY;
+            
+
+            mouseFetchX = e.X;
+            mouseFetchY = e.Y;
+
+            if (stavKreslenia == true)
+            {
+                grafika.DrawLine(coloredPen, mouseOldX, mouseOldY, mouseFetchX, mouseFetchY);
+            }
+            mouseOldX = mouseFetchX;
+            mouseOldY = mouseFetchY;
+
         }
 
-        
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            pictrOutput.Image = null;     
+        }
+
+        private void pictrOutput_MouseDown(object sender, MouseEventArgs e)
+        {
+            stavKreslenia = true;
+            mouseOldX = e.X;
+            mouseOldY = e.Y;
+            kresli(e);
+        }
+
+        private void pictrOutput_MouseUp(object sender, MouseEventArgs e)
+        {
+            stavKreslenia = false;
+        }
+
+        private void pictrOutput_MouseMove(object sender, MouseEventArgs e)
+        {      
+            kresli(e);         
+        }
+
+        private void buttonColor_Click(object sender, EventArgs e)
+        {
+            //workForm.ShowDialog();
+
+        }
     }
 }
