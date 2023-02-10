8aa53d46d23230cf6f446713a6de76ba2be326f9
augustinromaniak <augustin.romaniak88@zoznam.sk>
2023-02-10 11:33:24 +0100

    zaciatok prace nastal prave teraz

--- FormsApp/FormsApp/Form1.Designer.cs
+++ FormsApp/FormsApp/Form1.Designer.cs
@@ -28,27 +28,25 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnStart = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
             this.pictrOutput = new System.Windows.Forms.PictureBox();
-            this.buttonColor = new System.Windows.Forms.Button();
             this.colorDialog1 = new System.Windows.Forms.ColorDialog();
-            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.btnPaint = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictrOutput)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             this.SuspendLayout();
             // 
-            // btnStart
+            // btnDelete
             // 
-            this.btnStart.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
-            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.btnStart.ForeColor = System.Drawing.Color.IndianRed;
-            this.btnStart.Location = new System.Drawing.Point(12, 37);
-            this.btnStart.Name = "btnStart";
-            this.btnStart.Size = new System.Drawing.Size(138, 46);
-            this.btnStart.TabIndex = 0;
-            this.btnStart.Text = "Zmaž";
-            this.btnStart.UseVisualStyleBackColor = false;
-            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            this.btnDelete.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnDelete.ForeColor = System.Drawing.Color.IndianRed;
+            this.btnDelete.Location = new System.Drawing.Point(12, 37);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(138, 46);
+            this.btnDelete.TabIndex = 0;
+            this.btnDelete.Text = "Zmaž";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
             // 
             // pictrOutput
             // 
@@ -58,55 +56,36 @@
             this.pictrOutput.Size = new System.Drawing.Size(400, 400);
             this.pictrOutput.TabIndex = 1;
             this.pictrOutput.TabStop = false;
-            this.pictrOutput.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictrOutput_MouseDown);
-            this.pictrOutput.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictrOutput_MouseMove);
-            this.pictrOutput.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictrOutput_MouseUp);
             // 
-            // buttonColor
+            // btnPaint
             // 
-            this.buttonColor.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
-            this.buttonColor.ForeColor = System.Drawing.SystemColors.MenuHighlight;
-            this.buttonColor.Location = new System.Drawing.Point(21, 89);
-            this.buttonColor.Name = "buttonColor";
-            this.buttonColor.Size = new System.Drawing.Size(129, 117);
-            this.buttonColor.TabIndex = 2;
-            this.buttonColor.Text = "Farba\r\n";
-            this.buttonColor.UseVisualStyleBackColor = false;
-            this.buttonColor.Click += new System.EventHandler(this.buttonColor_Click);
-            // 
-            // trackBar1
-            // 
-            this.trackBar1.Location = new System.Drawing.Point(12, 278);
-            this.trackBar1.Minimum = 1;
-            this.trackBar1.Name = "trackBar1";
-            this.trackBar1.Size = new System.Drawing.Size(138, 45);
-            this.trackBar1.TabIndex = 3;
-            this.trackBar1.Value = 1;
-            this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
+            this.btnPaint.Location = new System.Drawing.Point(16, 120);
+            this.btnPaint.Name = "btnPaint";
+            this.btnPaint.Size = new System.Drawing.Size(134, 50);
+            this.btnPaint.TabIndex = 2;
+            this.btnPaint.Text = "Kresli";
+            this.btnPaint.UseVisualStyleBackColor = true;
+            this.btnPaint.Click += new System.EventHandler(this.btnPaint_Click);
             // 
             // Form1
             // 
             this.ClientSize = new System.Drawing.Size(596, 465);
-            this.Controls.Add(this.trackBar1);
-            this.Controls.Add(this.buttonColor);
+            this.Controls.Add(this.btnPaint);
             this.Controls.Add(this.pictrOutput);
-            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnDelete);
             this.Name = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictrOutput)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
 
-        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnDelete;
         private System.Windows.Forms.PictureBox pictrOutput;
-        private System.Windows.Forms.Button buttonColor;
         private System.Windows.Forms.ColorDialog colorDialog1;
-        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Button btnPaint;
     }
 }
 
--- FormsApp/FormsApp/Form1.cs
+++ FormsApp/FormsApp/Form1.cs
@@ -43,38 +43,16 @@ namespace FormsApp
 
         }
 
-        private void btnStart_Click(object sender, EventArgs e)
+        private void btnDelete_Click(object sender, EventArgs e)
         {
             pictrOutput.Image = null;     
         }
 
-        private void pictrOutput_MouseDown(object sender, MouseEventArgs e)
-        {
-            stavKreslenia = true;
-            mouseOldX = e.X;
-            mouseOldY = e.Y;
-            kresli(e);
-        }
 
-        private void pictrOutput_MouseUp(object sender, MouseEventArgs e)
-        {
-            stavKreslenia = false;
-        }
 
-        private void pictrOutput_MouseMove(object sender, MouseEventArgs e)
-        {      
-            kresli(e);         
-        }
-
-        private void buttonColor_Click(object sender, EventArgs e)
+        private void btnPaint_Click(object sender, EventArgs e)
         {
-            colorDialog1.ShowDialog();
-            coloredPen.Color = colorDialog1.Color;
-        }
 
-        private void trackBar1_ValueChanged(object sender, EventArgs e)
-        {
-            coloredPen.Width = trackBar1.Value;
         }
     }
 }
